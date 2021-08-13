d87a4ed225d165f789ece2c0a295ddb2b1a80fcf
Momen Sherif <momensherif.2019@gmail.com>
2021-08-13 19:49:43 +0200

    Add: Pagination

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import BasicTable from './components/1-Basic-table';
 import BasicTableMui from './components/1-Basic-table-MUI';
 import ColumnGroupTable from './components/2-column-group-and-footer';
 import SortingTable from './components/3-Sorting';
+import PaginatingTable from './components/4-Pagination';
 
 function App() {
   return (
     <div className="App">
-      <SortingTable />
+      <PaginatingTable />
     </div>
   );
 }
---
+++ src/components/4-Pagination.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import { useTable, useSortBy, usePagination } from 'react-table';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import data from '../data.json';
+
+import { columns } from './1-Basic-table';
+
+export default function PaginatingTable() {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize: 5 },
+    },
+    useSortBy,
+    usePagination,
+  );
+
+  return (
+    <>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage,
+            },
+            null,
+            2,
+          )}
+        </code>
+      </pre>
+      <Table {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  {column.render('Header')}
+                  <span style={{ position: 'absolute', marginLeft: 20 }}>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={previousPage} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={nextPage} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageCount}
+          </strong>{' '}
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
